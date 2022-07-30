dfc558c78897b78a868719182a0a3883d0bcec20
astakh <v.astakhov@gmail.com>
2022-07-30 16:18:13 +0300

    secur4

--- inter.js
+++ inter.js
@@ -78,12 +78,16 @@ async function market (exch) {
             let profit      = amountB * m[base + '/USDT'].bid - (3 * 0.1) - 100
 
             if (profit > 0.3 && profit < 10) {
-                await db.addDeal({exch: exch.name, profit: profit, type: 1, coin: coin, base: base})
-                console.log(`${coin}1: ${base} ${profit.toFixed(2)} : 100USDT => ${amountC.toFixed(4)}${coin} => ${amountB.toFixed(4)}${base} => ${(amountB * m[base + '/USDT'].bid).toFixed(2)}USDT`)
                 const avg1 = await avgTrade(exch, coin + "/USDT", 50, "asks")
                 const avg2 = await avgTrade(exch, coin + "/" + base, 50, "bids")
                 const avg3 = await avgTrade(exch, base + '/USDT', 50, "bids")
-                console.log(avg1, avg2, avg3, (100/avg1*avg2*avg3).toFixed(2))
+                profit     = (100/avg1*avg2*avg3) - 0.3
+                console.log(avg1, avg2, avg3, profit.toFixed(2))
+                if (profit > 0.3) {
+                    await db.addDeal({exch: exch.name, profit: profit, type: 1, coin: coin, base: base})
+                    console.log(`${coin}1: ${base} ${profit.toFixed(2)} : 100USDT => ${amountC.toFixed(4)}${coin} => ${amountB.toFixed(4)}${base} => ${(amountB * m[base + '/USDT'].bid).toFixed(2)}USDT`)
+    
+                }
             } 
 
             amountB     = 100 / m[base + '/USDT'].ask
@@ -91,12 +95,15 @@ async function market (exch) {
             profit      = amountC * m[coin + '/USDT'].bid - (3 * 0.1) - 100
 
             if (profit > 0.3 && profit < 10) {
-                await db.addDeal({exch: exch.name, profit: profit, type: 2, coin: coin, base: base})
-                console.log(`${coin}2: ${base} ${profit.toFixed(2)} : 100USDT => ${amountB.toFixed(4)}${base} => ${amountC.toFixed(4)}${coin} => ${(amountC * m[coin + '/USDT'].bid).toFixed(2)}USDT`)
                 const avg1 = await avgTrade(exch, base + "/USDT", 50, "asks")
                 const avg2 = await avgTrade(exch, coin + "/" + base, 50, "asks")
                 const avg3 = await avgTrade(exch, coin + '/USDT', 50, "bids")
-                console.log(avg1, avg2, avg3, (100/avg1/avg2*avg3).toFixed(2))
+                profit     = (100/avg1/avg2*avg3) - 0.3
+                console.log(avg1, avg2, avg3, profit.toFixed(2))
+                if (profit > 0.3) {
+                    await db.addDeal({exch: exch.name, profit: profit, type: 2, coin: coin, base: base})
+                    console.log(`${coin}2: ${base} ${profit.toFixed(2)} : 100USDT => ${amountB.toFixed(4)}${base} => ${amountC.toFixed(4)}${coin} => ${(amountC * m[coin + '/USDT'].bid).toFixed(2)}USDT`)
+                }
             }
 
         }
